1cdac6b3e097db053fc49954468707bcf2c12249
kyl3kan3 <kyl3kan3@gmail.com>
2025-06-03 18:27:21 -0500

    Add src/KeyQuestChallenge/postcss.config.tsx

---
+++ src/KeyQuestChallenge/postcss.config.tsx
@@ -0,0 +1,19 @@
+import React from 'react';
+
+// Converted from JavaScript
+export default {
+  plugins: {
+    tailwindcss: {},
+    autoprefixer: {},
+  },
+}
+
+
+export default function ConvertedComponent() {
+  return (
+    <div className="p-4">
+      <h1>Converted JavaScript Component</h1>
+      <p>Original code has been preserved above</p>
+    </div>
+  );
+}
