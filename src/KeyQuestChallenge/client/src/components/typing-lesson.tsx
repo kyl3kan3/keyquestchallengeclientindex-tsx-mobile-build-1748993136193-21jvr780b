f0d55f781001246c89614a54e3a03e737d38dc29
kyl3kan3 <kyl3kan3@gmail.com>
2025-06-03 18:27:33 -0500

    Add KeyQuestChallenge/client/src/components/typing-lesson.tsx

--- src/KeyQuestChallenge/client/src/components/typing-lesson.tsx
+++ src/KeyQuestChallenge/client/src/components/typing-lesson.tsx
@@ -1,3 +1,4 @@
+import React from "react";
 import { Clock, Heart, Pause, SkipForward, Lightbulb } from "lucide-react";
 import { Button } from "@/components/ui/button";
 import { Progress } from "@/components/ui/progress";
