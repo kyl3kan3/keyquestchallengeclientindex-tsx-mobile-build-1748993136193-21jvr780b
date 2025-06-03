5081b9b88089898fb1c926323f80f86e8c94462b
kyl3kan3 <kyl3kan3@gmail.com>
2025-06-03 18:28:10 -0500

    Add KeyQuestChallenge/client/src/pages/mini-game.tsx

--- src/KeyQuestChallenge/client/src/pages/mini-game.tsx
+++ src/KeyQuestChallenge/client/src/pages/mini-game.tsx
@@ -1,3 +1,4 @@
+import React from "react";
 import { useParams, useLocation } from "wouter";
 import { useQuery, useMutation } from "@tanstack/react-query";
 import { queryClient } from "@/lib/queryClient";
